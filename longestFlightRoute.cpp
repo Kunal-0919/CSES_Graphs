1810d8cef373b02725662e968a7efe87edd91951
Kunal <137786504+Kunal-0919@users.noreply.github.com>
2026-06-16 14:37:04 +0530

    Longest Flight Route

--- longestFlightRoute.cpp
+++ longestFlightRoute.cpp
@@ -1,136 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pb push_back
-#define MP make_pair
-#define rep(i, a, n) for (ll i = a; i <= n; i++)
-#define sq(a) (a) * (a)
-#define range(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-#define endl '\n'
-#define f first
-#define s second
+
 typedef long long ll;
-typedef long double lld;
-typedef unsigned long long ull;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<bool> vb;
-typedef vector<vector<ll>> vvl;
-typedef vector<vector<bool>> vvb;
-typedef set<ll> si;
-typedef set<ll> sl;
-typedef pair<ll, ll> pii;
-typedef pair<ll, ll> pll;
-typedef vector<pii> vpi;
-typedef vector<pll> vpl;
-typedef priority_queue<ll> maxHeap;
-typedef priority_queue<ll, vl, greater<ll>> minHeap;
-typedef map<ll, ll> mii;
-typedef map<string, ll> msi;
 
-void setIO(string name = "")
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin.tie(0)->sync_with_stdio(0);
-    cout.tie(0);
-    // if (sz(name))
-    // {
-    // freopen((name + ".in").c_str(), "r", stdin);
-    // freopen((name + ".out").c_str(), "w", stdout);
-    // }
-}
 
-const lld pi = 3.14159265358979323846;
-ll mod = 1e9 + 7;
+const ll MOD = 1e9 + 7;
+const ll INF = 9223372036854775807;
+#define FAST_EXECUTION
+#ifdef FAST_EXECUTION
+#pragma GCC optimize("O3")
+#endif
 
-ll n, m;
-vector<vl> g;
-vb vis;
-void dfs(ll n)
-{
-    vis[n] = 1;
-    for (auto x : g[n])
-    {
-        if (!vis[x])
-            dfs(x);
-    }
-}
-void solve()
-{
+void Main() {
+    ll n, m;
     cin >> n >> m;
-    g.assign(n, {});
-    vis.assign(n, 0);
-    for (ll i = 0; i < m; i++)
-    {
-        ll a, b;
-        cin >> a >> b;
-        a--, b--;
-        g[a].push_back(b);
-        // g[b].push_back(a);
+    vector<vector<ll>> g(n);
+    vector<ll> in(n);
+    for(ll i = 0;i < m;i++) {
+        ll a, b;cin >> a >> b; a--, b--;
+        g[a].push_back(b); in[b]++;
     }
-    dfs(0);
-    if (!vis[n - 1])
-    {
-        cout << "IMPOSSIBLE" << endl;
-        return;
-    }
-
-    for (ll i = 0; i < n; i++)
-    {
-        if (!vis[i])
-            g[i].clear();
-    }
-    vl in(n);
-    for (ll i = 0; i < n; i++)
-    {
-        for (auto x : g[i])
-            in[x]++;
-    }
+    // for(auto x : in) cout << x << " ";
+    // cout << endl;
     queue<ll> q;
-    q.push(0);
-    vl dist(n, LLONG_MAX);
-    dist[0] = 1;
-    vl par(n, -1);
-
-    while (!q.empty())
-    {
-        ll node = q.front();
-        q.pop();
-
-        for (auto x : g[node])
-        {
+    for(ll i = 0;i < n;i++) {
+        if(in[i]) continue;
+        q.push(i);
+    }
+    vector<ll> topo;
+    while(!q.empty()) {
+        ll curr = q.front();q.pop();
+        topo.push_back(curr);
+        for(auto x : g[curr]) {
             in[x]--;
-            if (!in[x])
-            {
-                dist[x] = 1 + dist[node];
-                q.push(x);
-                par[x] = node;
+            if(in[x]) continue;
+            q.push(x);
+        }
+    }
+    vector<ll>dist(n, LLONG_MIN);dist[0] = 0;
+    vector<ll> par(n, -2); par[0] = -1;
+    for(ll curr : topo) {
+        if(dist[curr] == LLONG_MIN) continue;
+        // cout << curr << " " << dist[curr] << endl;
+        for(auto x : g[curr]) {
+            if(dist[curr] + 1 > dist[x]) {
+                dist[x] = dist[curr] + 1;
+                par[x] = curr;
             }
         }
     }
-    cout << dist[n - 1] << endl;
-
-    ll node = n - 1;
-    vl ans;
-    while (node != -1)
-    {
-        ans.pb(node);
-        node = par[node];
+    
+    if(dist[n - 1] == LLONG_MIN) {
+        cout << "IMPOSSIBLE" << endl;return;
     }
-    reverse(range(ans));
-    for (auto x : ans)
-        cout << x + 1 << " ";
+    ll node = n - 1;
+    vector<ll> ans;
+    while(node != -1) {
+        ans.push_back(node);node = par[node];
+    }
+    cout << ans.size() << endl;
+    reverse(ans.begin(), ans.end());
+    for(auto x : ans) cout << x + 1 << " ";
     cout << endl;
 }
 
-int main()
-{
-    setIO();
-    ll t = 1;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
     // cin >> t;
-    while (t--)
-    {
-        solve();
+    while(t--) {
+        Main();
     }
+    return 0;
 }
